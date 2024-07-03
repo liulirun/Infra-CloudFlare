4db764b7248645cda8a4f161c1bc57b68dbf454f
liuli <liulirun@gmail.com>
2024-07-03 15:52:51 -0400

    import ruleset success

--- main.tf
+++ main.tf
@@ -8,12 +8,5 @@ terraform {
 }
 
 provider "cloudflare" {
-  api_token = "??"
-}
-
-data "cloudflare_rulesets" "example" {
-  zone_id = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
-  # filter {
-  #   name = ".*OWASP.*"
-  # }
+  api_token = var.cloudflare_api_token
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "cloudflare_api_token" {
+  description = "cloudflare api token"
+  type        = string
+  sensitive   = true
+}
