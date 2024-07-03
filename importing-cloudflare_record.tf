59ca5b848a09f5883e6b4fc48a5757066338f20d
liuli <liulirun@gmail.com>
2024-07-03 09:39:14 -0400

    first import

---
+++ importing-cloudflare_record.tf
@@ -0,0 +1,27 @@
+resource "cloudflare_record" "terraform_managed_resource_f70ccfb37a28a3767fdadfe284cfc13e" {
+  name    = "*"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "94.23.66.51"
+  zone_id = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_5902994c2febb8bdc5a5ed45ac5d50a1" {
+  name    = "liulirun.cloudns.ch"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "94.23.66.51"
+  zone_id = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_276a136e3c4f660e8885a6f66326b062" {
+  name    = "www"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "94.23.66.51"
+  zone_id = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
+}
+
