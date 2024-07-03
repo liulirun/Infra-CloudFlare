fb208000b442cbcf0cd9ae75571caeefccee64e6
liuli <liulirun@gmail.com>
2024-07-03 14:11:03 -0400

    ruleset

--- importing-cloudflare_custom_rules.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_firewall_rule" "terraform_managed_resource_d8fc57e348fe4ea9b82d5b7580c030f3" {
-  action      = "js_challenge"
-  description = "first_rule"
-  filter_id   = "18da4c224db14cfaba7cb4fbc460d608"
-  paused      = false
-  zone_id     = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
-}
-
--- importing-cloudflare_filter.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudflare_filter" "terraform_managed_resource_18da4c224db14cfaba7cb4fbc460d608" {
-  expression = "(ip.geoip.country eq \"AF\")"
-  paused     = false
-  zone_id    = "8f0b8c4abfb8b870fce5c65aa7c3f0d7"
-}
-
